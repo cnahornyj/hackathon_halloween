949633a338e017ff37bad5dee11e5f72bf400a1d
Cha-dieleman <chadieleman@gmail.com>
2019-10-30 19:55:36 +0100

    Mise en style footer et ajout des icones resseaux sociaux

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,12 @@
 import React, {Component} from 'react';
 import bg_footer from '../img/bg_footer.png';
-import { Container } from 'reactstrap';
+import youtube from '../img/youtube.png';
+import twitter from '../img/twitter.png';
+import instagram from '../img/instagram.png';
+import facebook from '../img/facebook.png';
+import { Container, NavLink } from 'reactstrap';
+import './footer.css';
+import '../style.css';
 
 class Footer extends Component {
     constructor(props){
@@ -10,21 +16,27 @@ class Footer extends Component {
 
     render(){
         return(
-            <footer>
-                <Container >
-                    <img src={bg_footer} alt="bg-footer" />
-                    <div>
-                        <div>icones réseaux sociaux</div>
-                        <div>
-                            <p>Mes monstres</p>
-                            <p>Actu de l'horreur</p>
-                            <p>Contact</p>
-                            <p>Mentions légales</p>
+            <div>
+                <img src={bg_footer} alt="bg-footer" className="imgInclineeFooter" />
+                <footer>
+                    <Container >
+                        <div className="pourFlex">
+                            <div className="flexReseauxSociaux">
+                                <div><img src={youtube} className="cssImgIcones"/></div>
+                                <div><img src={facebook} className="cssImgIcones"/></div>
+                                <div><img src={instagram} className="cssImgIcones"/></div>
+                                <div><img src={twitter} className="cssImgIcones"/></div>
+                            </div>
+                            <ul className="cssDesLiens">
+                                <li><NavLink>MES MONSTRES</NavLink></li>
+                                <li><NavLink>ACTU DE L'HORREUR</NavLink></li>
+                                <li><NavLink>CONTACT</NavLink></li>
+                                <li><NavLink>MENTIONS LEGALES</NavLink></li>
+                            </ul>
                         </div>
-                    </div>
-                </ Container>
-            </footer>
-            
+                    </ Container>
+                </footer>
+            </div>
         )
     }
 }
