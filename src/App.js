b6c796dc9c776a828772a504d372696f0921065a
Arm <armengaud.m@gmail.com>
2019-10-30 22:03:51 +0100

    Page ListMatches STABLE missing avatar img

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React,{ Component} from 'react';
 import Accueil from './pages/Accueil';
 import Footer from './components/Footer';
+import ListMatches from './pages/ListMatches';
 
 class App extends Component {
-   
-
   render(){
     return(
       <div>
