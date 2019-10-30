e54b54056583fe199621bd98d55d3fec5492bb9f
Cha-dieleman <chadieleman@gmail.com>
2019-10-30 16:19:23 +0100

    creation composant Nav1

---
+++ src/components/Nav1.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import bg_footer from '../img/bg_footer.png';
+
+
+class Nav1 extends Component {
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Nav1;
