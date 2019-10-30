1740ffc1d2626289e5a85f9a226a445cbf1e8f54
Cha-dieleman <chadieleman@gmail.com>
2019-10-30 15:01:01 +0100

    construction squelette footer

---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import bg_footer from '../images/bg_footer.png';
+
+
+class Footer extends Component {
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+    render(){
+        return(
+            <footer>
+                <img src={bg-footer} alt="bg-footer" />
+                <div>
+                    <div></div>
+                    <div>
+                        <p>Mes monstres</p>
+                        <p>Actu de l'horreur</p>
+                        <p>Contact</p>
+                        <p>Mentions légales</p>
+                    </div>
+                </div>
+            </footer>
+        )
+    }
+}
+
+export default Footer;
