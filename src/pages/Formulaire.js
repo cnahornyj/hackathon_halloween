9e4413bc543f0c5631713fc398cfd0dc80d837fd
Arm <armengaud.m@gmail.com>
2019-10-31 10:09:33 +0100

    style for page Formulaire and DisplayMovies + axios for avatar monsters

--- src/pages/Formulaire.js
+++ src/pages/Formulaire.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Form, FormGroup, Label, Input, Col } from 'reactstrap';
+import { Container, Row,   Form, FormGroup, Label, Input, Col } from 'reactstrap';
 import DisplayMovies from '../components/DisplayMovies';
 import axios from "axios";
 import Nav2  from '../components/Nav2'
+import '../components/Formulaire.css'
 
 
 class Formulaire extends React.Component {
@@ -37,91 +38,110 @@ class Formulaire extends React.Component {
   return (
     <div>
       <Nav2 />
-      <Form>
-        <FormGroup>
-          <Label for="exampleEmail">Choisissez votre avatar monstre :</Label>
-          <Input type="textarea" name="text" id="exampleText" />
-        </FormGroup>
+      <div  className="formulaire">
+      <Container>
+        <h1 className="color_green">Créez votre compte</h1>
+        <Form>
+          <Row>
+            <Col sm={6}>
+            <FormGroup>
+              <Label for="exampleEmail">Choisissez votre avatar monstre :</Label>
+            </FormGroup>
+            </Col>
 
-        <FormGroup>
-          <Label for="exampleEmail">Pseudo :</Label>
-          <Input type="textarea" name="text" id="exampleText" />
-        </FormGroup>
-        
-        <FormGroup tag="fieldset" row>
-          <legend className="col-form-label col-sm-2">Je suis :</legend>
-          <Col sm={10}>
-            <FormGroup check>
-              <Label check>
-                <Input type="radio" name="radio2" />{' '}
-                Un homme
-              </Label>
-            </FormGroup>
-            <FormGroup check>
-              <Label check>
-                <Input type="radio" name="radio2" />{' '}
-                Une femme
-              </Label>
-            </FormGroup>
+            <Col sm={6}>
+          <FormGroup>
+            <Label for="exampleEmail">Pseudo :</Label>
+            <Input type="text" name="text" id="exampleText" />
+          </FormGroup>
+          <FormGroup tag="fieldset">
+
+            <legend className="col-form-label">Je suis :</legend>
+          <Row>
+            <Col>
+              <FormGroup check>
+                <Label check>
+                  <Input type="radio" name="radio2" />{' '}
+                  Un homme
+                </Label>
+              </FormGroup>
+              </Col>
+              <Col>
+              <FormGroup check>
+                <Label check>
+                  <Input type="radio" name="radio2" />{' '}
+                  Une femme
+                </Label>
+              </FormGroup>
+            </Col>
+            </Row>
+          </FormGroup>
+
+          <FormGroup tag="fieldset">
+            <legend className="col-form-label">Je recherche :</legend>
+            <Row>
+            <Col>
+              <FormGroup check>
+                <Label check>
+                  <Input type="radio" name="radio2" />{' '}
+                  Un homme
+                </Label>
+              </FormGroup>
+              </Col>
+              <Col>
+              <FormGroup check>
+                <Label check>
+                  <Input type="radio" name="radio2" />{' '}
+                  Une femme
+                </Label>
+              </FormGroup>
+              </Col>
+              <Col>
+              <FormGroup check>
+                <Label check>
+                  <Input type="radio" name="radio2" />{' '}
+                  Les deux
+                </Label>
+              </FormGroup>
+            </Col>
+            </Row>
+
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleEmail">Mon âge :</Label>
+            <Input type="select" name="select" id="exampleSelect">
+            <option>Entre 18 et 29 ans</option>
+            <option>Entre 30 et 44 ans</option>
+            <option>Entre 45 et 59 ans</option>
+            <option>Entre 60 et 74 ans</option>
+            <option>75 ans et (+)</option>
+            </Input>
+          </FormGroup>
+
+          <FormGroup>
+            <Label for="exampleEmail">Mon type de films d’horreurs préféré :</Label>
+            <Input type="select" name="select" id="exampleSelect">
+            <option>Bogeyman</option>
+            <option>Comédie horrifique</option>
+            <option>Epouvante</option>
+            <option>Fantastique</option>
+            <option>Final girl</option>
+            <option>Found footage</option>
+            <option>Gore</option>
+            <option>Jump scare</option>
+            <option>Scream queen</option>
+            <option>Slasher</option>
+            <option>Snuff movie</option>
+            <option>Survival</option>
+            <option>Thriller</option>
+            <option>Zombie</option>
+            </Input>
+          </FormGroup>
           </Col>
-        </FormGroup>
-
-        <FormGroup tag="fieldset" row>
-          <legend className="col-form-label col-sm-2">Je recherche :</legend>
-          <Col sm={10}>
-            <FormGroup check>
-              <Label check>
-                <Input type="radio" name="radio2" />{' '}
-                Un homme
-              </Label>
-            </FormGroup>
-            <FormGroup check>
-              <Label check>
-                <Input type="radio" name="radio2" />{' '}
-                Une femme
-              </Label>
-            </FormGroup>
-            <FormGroup check>
-              <Label check>
-                <Input type="radio" name="radio2" />{' '}
-                Les deux
-              </Label>
-            </FormGroup>
-          </Col>
-        </FormGroup>
-
-        <FormGroup>
-          <Label for="exampleEmail">Mon âge :</Label>
-          <Input type="select" name="select" id="exampleSelect">
-          <option>Entre 18 et 29 ans</option>
-          <option>Entre 30 et 44 ans</option>
-          <option>Entre 45 et 59 ans</option>
-          <option>Entre 60 et 74 ans</option>
-          <option>75 ans et (+)</option>
-          </Input>
-        </FormGroup>
-
-        <FormGroup>
-          <Label for="exampleEmail">Mon type de films d’horreurs préféré :</Label>
-          <Input type="select" name="select" id="exampleSelect">
-          <option>Bogeyman</option>
-          <option>Comédie horrifique</option>
-          <option>Epouvante</option>
-          <option>Fantastique</option>
-          <option>Final girl</option>
-          <option>Found footage</option>
-          <option>Gore</option>
-          <option>Jump scare</option>
-          <option>Scream queen</option>
-          <option>Slasher</option>
-          <option>Snuff movie</option>
-          <option>Survival</option>
-          <option>Thriller</option>
-          <option>Zombie</option>
-          </Input>
-        </FormGroup>
-      </Form>
-
+          </Row>
+        </Form>
+      </Container>
+      </div>
       {this.state.movies ? (
             <DisplayMovies movies={this.state.movies} />
           ) : (
