6015138ba4825662f4ee703d2538f7fdb1ee9615
stéphane <gervaisstephane@orange.fr>
2019-10-31 10:41:20 +0100

    toto

--- src/components/DisplayMovies.js
+++ src/components/DisplayMovies.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import '../components/DisplayMovies.css'
 import '../style.css'
-import { Container, Button, Row } from 'react-bootstrap'
 
 function DisplayMovies({ movies }){
     return(
         movies.map((movie, index) => (
             <div key={index} className="bock-movies">
              <div className="movies_picture">
-                    <img src={ movies.posterUrl } alt="poster"/>
+                    <img src={ movie.posterUrl } alt="poster"/>
                     <button className="like_movies"></button>
                 </div>
             <div className="infos_movies">
-                <h3>{movies.title}</h3>
-                <p>Réalisateur : {movies.director}</p>
-                <p>Date de sortie : {movies.year}</p>
+                <h3>{movie.title}</h3>
+                <p>Réalisateur : {movie.director}</p>
+                <p>Date de sortie : {movie.year}</p>
             </div>
             </div>    
         ))
