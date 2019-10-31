8670563b31f54c1fafe0e0314c0e3c7b63465f7d
Morgane Mottey <morgane.mottey@gmail.com>
2019-10-31 10:21:41 +0100

    version final2

--- src/components/Content.js
+++ src/components/Content.js
@@ -21,25 +21,23 @@ class Accueil extends Component {
         <Col sm={4}>
           <h1>ADOPTE UN MONSTRE</h1>
           <p> Le premier site de rencontre pour tous les amateurs de films d'horreurs</p>
-          
           <Link to="/formulaireDeConnexion"><button> Je m'inscris </button></Link>
           </Col>
         </Container>
         </div>
-        <div>
-        <img className="img_footer" src= {bg_footer2}></img>
-        </div>
 
-        <div className="accueil">
+        <div>
+        <img className="bg_footer" src= {bg_footer2}></img>
+        </div>
+        
+        <div className="accueil2">
         <Container className="suivre">
         <Row>
         <Col sm={4}>
+        
         <Card>
-            
             <CardBody className="image">
-            <div className = "circle">
             <CardImg className ="img1"  src={picto_suivez}alt="" />
-            </div>
               <h3>Suivez</h3>
               <CardText>L'actualité des meilleurs films d'horreurs</CardText>
             </CardBody>
@@ -48,12 +46,10 @@ class Accueil extends Component {
 
           <Col sm={4}>
           <Card>
-            <CardBody className="image">
-              <div className = "circle">
+            <CardBody className="image">  
             <CardImg className ="img2" src = {picto_trouvez}alt="" />
-            </div>
               <h3>Trouver</h3>
-              <CardText>L'actualité des meilleurs films d'horreurs</CardText>
+              <CardText>L'âme soeur en quelques heures</CardText>
             </CardBody>
           </Card>
           </Col>
@@ -61,21 +57,18 @@ class Accueil extends Component {
           <Col sm={4}>
           <Card>
             <CardBody className="image">
-            <div className = "circle">
             <CardImg className ="img3" src = {picto_rencontrez}alt="" />
-            </div>
               <h3>Rencontrez </h3>
               <CardText>Les monstres dans des lieux insolites</CardText>
             </CardBody>
           </Card>
-          </Col>
-
-         
+          </Col> 
         </Row>
         </Container>
         </div>
           
       <Container className="partenaires">
+  
       <h4> Nos partenaires</h4>
         <div className = "movie">
           <img className="img_movie" src="https://www.stickpng.com/assets/images/580b57fcd9996e24bc43c529.png" alt="netflix"></img>
@@ -83,6 +76,7 @@ class Accueil extends Component {
           <img className="img_movie" src="https://upload.wikimedia.org/wikipedia/fr/thumb/f/f8/Path%C3%A9_Logo.svg/1280px-Path%C3%A9_Logo.svg.png" alt="pathe"></img>
           <img className="img_movie" src="https://files.slack.com/files-pri/T6SG2QGG2-FQ1JDMU4F/la-nuit-de-l_horreur.png" alt="pathe"></img>
           </div>
+     
         </Container>
 
 
